fac8bd88e15503bbdeb0b867d4d60ee6556d65fa
Ruben <rubenpereira.wx@gmail.com>
2023-12-01 15:38:30 +0000

    refator

--- test/test.js
+++ test/test.js
@@ -1,9 +1,10 @@
-const { Builder, until, By, Select } = require("selenium-webdriver");
-const { elementIsVisible } = require("selenium-webdriver/lib/until");
+const { Builder } = require("selenium-webdriver");
 const navigators = require("../pages/navigators");
 const screenChecks = require("../pages/screenChecks");
+const chai = require("chai");
+const fileOperations = require("../fileOperations");
 
-describe('Array', async function () {
+describe('Validate the negative path of enquiring the highest price at Mercedes-Benz', function () {
     const timeoutInMinutes = 30;
     this.timeout(timeoutInMinutes * 60000);
     
@@ -11,45 +12,34 @@ describe('Array', async function () {
 
     before(async function() {
         driver = await new Builder().forBrowser('chrome').build();
-        //await driver.manage().window().maximize();
     });
     
     it('Open the Mercedes-Benz shop', async function() {
        await driver.get('https://shop.mercedes-benz.com/en-au/shop/vehicle/srp/demo');
        await screenChecks.checkPageTitle(driver, 'Search Overview');
-       
-       await new Promise(resolve => setTimeout(resolve, 4000));
 
        await navigators.pressAcceptAllCookiesButton(driver);
-
    });
    
     it('Fill location info', async function() {
         await navigators.selectStateOption(driver, 'New South Wales');
-        await new Promise(resolve => setTimeout(resolve, 1000));
-        await navigators.insertPostalCode(driver, '2007');
-        await new Promise(resolve => setTimeout(resolve, 1000));
+        await navigators.insertLocationPostalCode(driver, '2007');
         await navigators.pressPrivateRadioButton(driver);
         await navigators.pressModalCloseButton(driver);
-        await new Promise(resolve => setTimeout(resolve, 7000));
     });
 
     it('Click the filter button', async function() {
         await navigators.pressFilterButton(driver);
-        await new Promise(resolve => setTimeout(resolve, 5000));
     });
 
     it('Select a color from Pre-Owned tab', async function() {
         await navigators.clickPreOwnTab(driver);
-        await new Promise(resolve => setTimeout(resolve, 12000));
         
         await navigators.pressFilterButton(driver);
-        await new Promise(resolve => setTimeout(resolve, 10000));
         
         await navigators.clickOnColorFilter(driver);
-        await new Promise(resolve => setTimeout(resolve, 1000));
-
         await navigators.selectColorOption(driver);
+        
         await navigators.closeFilterModal(driver);
     });
     
@@ -59,19 +49,30 @@ describe('Array', async function () {
     });
 
     it('Save car details to a file', async function() {
-        await driver.manage().setTimeouts({ implicit: 3000 });
-        await screenChecks.getModelYear(driver);
-        await screenChecks.getVin(driver);
+        const modelYear = await screenChecks.getModelYear(driver);
+        const vin = await screenChecks.getVin(driver);
+
+        fileOperations.saveToFile(`Model Year: ${modelYear}\nVIN: ${vin}`);
     });
     
     it('Click on Enquire Now button', async function() {
         await navigators.clickOnEnquireNow(driver);
     });
     
-    it('Fill contact details with wrong email', async function() {
-        await navigators.insertEmail(driver, 'a.com');
-        await new Promise(resolve => setTimeout(resolve, 20000));
-    })
+    it('Fill contact details with invalid email', async function() {
+        await navigators.insertFirstName(driver, 'Vitor');
+        await navigators.insertLastName(driver, 'Antunes');
+        await navigators.insertEmail(driver, 'abc');
+        await navigators.insertPhoneNumber(driver, '0441234567');
+        await navigators.insertPostalCode(driver, "2007");
+    });
+    
+    it('Click Proceed and validate the error', async function() {
+        await navigators.clickOnProceedButton(driver);
+        const error = await screenChecks.getErrorMessage(driver);
+
+        chai.assert.equal(error, 'An error has occurred.\nPlease check the following sections:');
+    });
 
     after(async function() {
         await driver.quit();

ee94ae5bc14e2eac24c894cc49c3a671c7513b1d
rubenpereira3 <rubenpereira3@users.noreply.github.com>
2023-12-01 11:22:06 +0000

    add new steps

--- pages/navigators.js
+++ pages/navigators.js
@@ -53,6 +53,29 @@ async function closeFilterModal(driver) {
     await driver.findElement(By.xpath(closeBtnLocator)).click();
 }
 
+async function selectFilterOption(driver, value) {
+    const selectLocator = '/html/body/div/div[1]/main/div[2]/div[1]/div[2]/div[2]/div[2]/wb-select-control/wb-select/select';
+    const selectElement = await driver.wait(until.elementIsVisible(driver.findElement(By.xpath(selectLocator))), timeout);
+    const select = new Select(selectElement);
+
+    await select.selectByValue(value);
+}
+
+async function clickOnFirstResult(driver, value) {
+    const locator = 'dcp-cars-srp-results__tile';
+    await driver.findElement(By.className(locator)).click();
+}
+
+async function clickOnEnquireNow(driver) {
+    const locator = '[data-test-id="dcp-buy-box__contact-seller"]';
+    await driver.findElement(By.css(locator)).click();
+}
+
+async function insertEmail(driver, email) {
+    const locator = '/html/body/div/div[1]/main/div/div[1]/div[7]/div/div[1]/div/div[2]/div/div/div/div/div[1]/form/div/div[3]/wb-input-control/wb-input/input';
+    await driver.findElement(By.xpath(locator)).sendKeys(email);
+}
+
 module.exports = {
     pressAcceptAllCookiesButton,
     selectStateOption,
@@ -63,5 +86,9 @@ module.exports = {
     clickPreOwnTab,
     clickOnColorFilter,
     selectColorOption,
-    closeFilterModal
+    closeFilterModal,
+    selectFilterOption,
+    clickOnFirstResult,
+    clickOnEnquireNow,
+    insertEmail
 };

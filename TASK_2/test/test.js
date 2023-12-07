9024132ccf1a28988e9fd9e3d58f7ec76d40bcb7
Ruben <rubenpereira.wx@gmail.com>
2023-12-07 22:18:51 +0000

    feat: Replace it's by steps

--- TASK_2/test/test.js
+++ TASK_2/test/test.js
@@ -3,7 +3,7 @@ const navigators = require("../pages/navigators");
 const screenChecks = require("../pages/screenChecks");
 const chai = require("chai");
 const fileOperations = require("../fileOperations");
-const { parseHttpResponse } = require("selenium-webdriver/http");
+const { step } = require("mocha-steps");
 
 describe('Validate the negative path of enquiring the highest price at Mercedes-Benz', function () {   
     const timeoutInMinutes = 30;
@@ -18,25 +18,25 @@ describe('Validate the negative path of enquiring the highest price at Mercedes-
         driver = await new Builder().forBrowser(browser).build();
     });
     
-    it('Open the Mercedes-Benz shop', async function() {
+    step('Open the Mercedes-Benz shop', async function() {
        await driver.get('https://shop.mercedes-benz.com/en-au/shop/vehicle/srp/demo');
        await screenChecks.checkPageTitle(driver, 'Search Overview');
 
        await navigators.pressAcceptAllCookiesButton(driver);
    });
    
-    it('Fill location info', async function() {
+    step('Fill location info', async function() {
         await navigators.selectStateOption(driver, 'New South Wales');
         await navigators.insertLocationPostalCode(driver, '2007');
         await navigators.pressPrivateRadioButton(driver);
         await navigators.pressModalCloseButton(driver);
     });
 
-    it('Click the filter button', async function() {
+    step('Click the filter button', async function() {
         await navigators.pressFilterButton(driver);
     });
 
-    it('Select a color from Pre-Owned tab', async function() {
+    step('Select a color from Pre-Owned tab', async function() {
         await navigators.clickPreOwnTab(driver);
         
         await navigators.pressFilterButton(driver);
@@ -47,23 +47,23 @@ describe('Validate the negative path of enquiring the highest price at Mercedes-
         await navigators.closeFilterModal(driver);
     });
     
-    it('Navigate to the Vehicle Details of the most expensive car', async function() {
+    step('Navigate to the Vehicle Details of the most expensive car', async function() {
         await navigators.selectFilterOption(driver, 'price-desc-ucos');
         await navigators.clickOnFirstResult(driver);
     });
 
-    it('Save car details to a file', async function() {
+    step('Save car details to a file', async function() {
         const modelYear = await screenChecks.getModelYear(driver);
         const vin = await screenChecks.getVin(driver);
 
         fileOperations.saveToFile(`Model Year: ${modelYear}\nVIN: ${vin}`);
     });
     
-    it('Click on Enquire Now button', async function() {
+    step('Click on Enquire Now button', async function() {
         await navigators.clickOnEnquireNow(driver);
     });
     
-    it('Fill contact details with invalid email', async function() {
+    step('Fill contact details with invalid email', async function() {
         await navigators.insertFirstName(driver, 'Vitor');
         await navigators.insertLastName(driver, 'Antunes');
         await navigators.insertEmail(driver, 'abc');
@@ -71,7 +71,7 @@ describe('Validate the negative path of enquiring the highest price at Mercedes-
         await navigators.insertPostalCode(driver, "2007");
     });
     
-    it('Click Proceed and validate the error', async function() {
+    step('Click Proceed and validate the error', async function() {
         await navigators.clickOnProceedButton(driver);
         const error = await screenChecks.getErrorMessage(driver);
 
